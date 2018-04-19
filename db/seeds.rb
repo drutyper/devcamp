da21488beed52151a02d82fa6db28e5f6e0186ee
dru <drutyper@gmail.com>
2018-04-19 19:49:38 -0400

    Created seed file for sample date

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: Faker::RickAndMorty.location,
+		body: Faker::RickAndMorty.quote
+	)
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+	Skill.create!(
+		title: Faker::FamilyGuy.quote,
+		percent_utilized: 30
+	)
+end
+
+puts "5 skills created"
+
+9.times do |portfolio|
+	Portfolio.create!(
+		title: Faker::GameOfThrones.house,
+		subtitle: Faker::Movie.quote,
+		body: Faker::BackToTheFuture.quote,
+		main_image: "http://placehold.it/600x400",
+		thumb_image: "http://placehold.it/350x200"
+	)
+end
+
+
+puts "9 portfolio items"
